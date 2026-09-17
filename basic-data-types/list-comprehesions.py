174b9fde70be5d36958eff9dc9d202f96e61adb6
Jubaea chilensis <guillermo.vidal.astudillo@gmail.com>
2026-09-17 00:48:39 -0300

    entendiendo objetivo del desafio

--- basic-data-types/list-comprehesions.py
+++ basic-data-types/list-comprehesions.py
@@ -4,5 +4,14 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 
-# no entiendo lo que es un couboide, ya ahora si es un paralelepipedo. 
-# no entiendo el objetivo que son x,y y z 
+"""
+- Cuboide: caja con forma de paralelepípedo.
+- x, y, z: tamaño de la caja (largo, fondo, alto). Son fijos.
+- i, j, k: coordenadas de un punto. Van cambiando.
+- Grilla: todos los puntos con coordenadas enteras dentro de la caja.
+    - Ej.: x=2, y=1, z=1 -> 12 puntos (8 esquinas + 4 intermedios).
+- Regla 1: recorrer la grilla -> i de 0 a x, j de 0 a y, k de 0 a z.
+- Regla 2: descartar los puntos donde i + j + k == n.
+    - Ej.: [2, 0, 0] con n=2 -> 2+0+0 = 2 -> se descarta.
+"""
+# con bucles para entender la logica 
