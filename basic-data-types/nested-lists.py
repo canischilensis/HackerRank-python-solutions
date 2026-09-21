48562dd122c89497a6f50caa15f97ba4f0bc1282
diadasiachilensis <diadasiachilensis@github.com>
2026-09-21 14:25:34 -0300

    me di cuenta del error, debo cambiar tipo con map() e int

--- basic-data-types/nested-lists.py
+++ basic-data-types/nested-lists.py
@@ -4,6 +4,14 @@
 entregan el entero en n 
 la seugnda linea contiene n enteros separados por espacios describiendo los elementos de la tupla. 
 
+
+if __name__ == '__main__':
+    n = input().split()
+    t = tuple(list(input().split()))
+    print(hash(t))
+
+cree lo anterior pero no obtuve el otuput. porque no cambie los valores, recordar siempre entran 
+
 """
 
 # no aparece nada en el codigo entonces empiezo a hacer lo mismo
